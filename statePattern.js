9650886d26fd33f75ba0dc2de933cba3f9f10445
sids4ru <sids4ru@gmail.com>
2015-03-21 16:31:54 +0530

    State Machine
    Very simple and good in performance implementation of State machine

---
+++ statePattern.js
@@ -0,0 +1,52 @@
+function _State(object) {
+	if (object === undefined) {
+		object = new function () {
+			this.name = "NO_STATE",
+			this.onEnter = function (data) {},
+			this.onExit = function (data) {},
+			this.next = function (name, data) {
+				return name;
+			},
+			this.update = function (data) {};
+		};
+
+	};
+	this.name = object.name;
+	if (this.name === undefined)
+		console.log("Give name");
+	this.onEnter = object.onEnter;
+	if (this.onEnter === undefined)
+		console.log("Give onEnter")
+		this.onExit = object.onExit;
+	if (this.onExit === undefined)
+		console.log("Give onExit");
+	this.next = object.next;
+	if (this.next === undefined)
+		console.log("Give next(name)");
+	this.update = object.update;
+	if (this.update === undefined)
+		console.log("give update");
+}
+function _StateMachine() {
+	var states = [];
+	var current = states["NO_STATE"] = new _State();
+	this.setState = function (name, data) {
+		if (current.name === name) {
+			return current.update(data);
+		}
+		prev = current;
+		current = states[current.next(name, data)];
+		if (current === prev) {
+			return current.update(data);
+		}
+		prev.onExit(data);
+		if (current === undefined) {
+			current = states["NO_STATE"];
+		}
+		current.onEnter(data);
+		return current.update(data);
+	}
+	this.addState = function (st) {
+		states[st.name] = st;
+	}
+}
